047252036df118f3a6831bdfbed5451d4f1122dc
Tim <thaines2@wisc.edu>
2013-08-08 14:26:13 -0500

    uncommented lines from previous commit

--- pm.c
+++ pm.c
@@ -22,12 +22,10 @@ int main(){
 	unlink("pos.dat"); unlink("density.dat");
 	init();
 	ic(a);
-//	printf("init done!\n");
-//	update_density(shape,a);
-//	printf("density updated!\n");
+	printf("init done!\n");
+	update_density(shape,a);
+	printf("density updated!\n");
 	output(a,"pos.dat","density.dat");
-
-	cleanup(); return 0;
 
 	int n;
 	for(n=0;n<10;n++) {
