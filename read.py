878affd64024eb4c98c1b675e2abed5867e28aac
Sarah B <sarah.benjamin@yale.edu>
2013-08-12 10:26:57 -0500

    modified to take arguments

--- read.py
+++ read.py
@@ -2,168 +2,102 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
+import sys
 
-def readpos():
+def readpos(gridsize,n_particles_1d,a_initial,delta_a,total_steps,dt=1):
     f = open('pos.dat','r')
 
     plt.clf()
     fig = plt.figure(1)
     sp = fig.add_subplot(111)
     
-    colors = iter(cm.rainbow(np.linspace(0,1,20)))
-    length = 1000 - 8
-    #length = 64*64*64 - 62
-    length = 32*32*32 - 30
+    n_steps = total_steps/dt
+    colors = iter(cm.rainbow(np.linspace(0,1,n_steps)))
+    length = (n_particles_1d)**3 - (n_particles_1d - 2)
 
-
-    DELTA_A  = 0.02
     PI = 3.14159
-    H_0 = 72
-    A_INITIAL = 0.2
-    a_cross = 10.0 * A_INITIAL
-    Np = 32
-    L_BOX = 64.0
-    k = 2 * PI / 64.0
-    D_aini = 1
+    a_cross = 10.0 * a_initial
+    nump = float(n_particles_1d)
+    l_box = float(gridsize)
+    k = 2 * PI / l_box
     amp = 1.0 / (a_cross * k)
     OMEGA_M = 1.0
     OMEGA_L = 0.0
-    r_0 = (10.0*3.086e16)/32.0
 
-    a_temp = A_INITIAL - (DELTA_A / 2.0)
-    D_dot =(math.sqrt(OMEGA_M + OMEGA_L * a_temp**3.0) / math.sqrt(a_temp));
+    a_half = a_initial - (delta_a/ 2.0)
+    D_dot =(math.sqrt(OMEGA_M + OMEGA_L * a_half**3.0) / math.sqrt(a_half));
    
-    temp_a = A_INITIAL
-    for dt in range(200):
+    a = a_initial
+    for t in range(total_steps):
         x = []
         v = []
-        y = []
         for i in range(7):
-            a = f.readline()
+            line = f.readline()
         for i in range(32):
-            a = f.readline()
-            data = a.split()
+            line = f.readline()
+            data = line.split()
             x.append(data[0])
             v.append(data[3])
         for i in range(length):
-            a = f.readline()
+            line = f.readline()
        
         xtest = []
         vtest = []
-        for i in range(32):
-            checkx = (float(i)*(L_BOX/Np) + temp_a*amp*math.sin(k*float(i)*(L_BOX/Np)))
+        for i in range(n_particles_1d):
+            checkx = (float(i)*(l_box/nump) + a*amp*math.sin(k*float(i)*(l_box/nump)))
             xtest.append(checkx)
-            temp_a_half = temp_a - (DELTA_A/2.0)
+            temp_a_half = a - (delta_a/2.0)
             D_dot =(math.sqrt(OMEGA_M + OMEGA_L * temp_a_half**3.0) / math.sqrt(temp_a_half));
-            checkv = temp_a_half*temp_a_half*D_dot*amp*math.sin(k*float(i)*(L_BOX/Np))
+            checkv = temp_a_half*temp_a_half*D_dot*amp*math.sin(k*float(i)*(l_box/nump))
             vtest.append(checkv)
-        
 
-        lx = 64
-        lv = 8.0*(float(dt)/40.0)
-
-        if (dt%10 == 0):
+        if (t%dt == 0):
             c = next(colors)
             sp.plot(x,v,'o',color=c)
             sp.plot(xtest,vtest,'-',color=c)
-            print temp_a,dt
-            a = "a = "+str(temp_a)
-            plt.text(lx,lv,a,color=c)
-        temp_a = temp_a + DELTA_A
-        #print temp_a,dt
+            print a,t
 
-        
+        a = a + delta_a
+           
     plt.show()
     #plt.savefig("ZA_pos.jpeg")
     f.close()
 
-    xg = []
-    f = open('density.dat','r')
-    a = f.readline()
-    a = f.readline()
-    data = a.split()
-    dens = []
-    for i in range(32):
-        step = (10.0/32.0)*float(i)
-        xg.append(step)
-        dens.append(data[i])
-    
-    f.close()
-
-def readdens():
+def readdens(gridsize,a_initial,delta_a,total_steps,dt=1):
     f = open('density.dat','r')
 
     plt.clf()
     fig = plt.figure(1)
     sp = fig.add_subplot(111)
-
-    #testing123
     
-    colors = iter(cm.rainbow(np.linspace(0,1,20)))
-    length = 1000 - 1
-    length = 32*32*32 - 30
+    n_steps = total_steps/dt
+    colors = iter(cm.rainbow(np.linspace(0,1,n_steps)))
+    length = gridsize*gridsize + 1
 
-    for dt in range(4):
+    a = a_initial
+    for t in range(total_steps):
         x = []
-        a = f.readline()
-        a = f.readline()
-        data = a.split()
         dens = []
-        for i in range(32):
+        line = f.readline()
+        line = f.readline()
+        data = line.split()
+        for i in range(gridsize):
             x.append(i)
             dens.append(data[i])
-        for i in range(31*32):
-            a = f.readline()
+        for i in range(length):
+            line = f.readline()
         
-        lx = 9
-        lv = max(dens)
-        
-        c = next(colors)
-        sp.plot(x,dens,'o',color=c)
-        sp.plot(x,dens,'-',color=c)
+        if (t%dt == 0):
+            c = next(colors)
+            sp.plot(x,dens,'o',color=c)
+            sp.plot(x,dens,'-',color=c)
+            print a,t
+            
+        a = a + delta_a
        
     plt.show()
     f.close()
     
-def checkdensity():
-    f = open('density.dat','r')
 
-    length = 32*32*32 - 30
-
-    for dt in range(1):
-        x = []
-        a = f.readline()
-       
-        dens = []
-        total = 0.0
-        for i in range(32):
-            for j in range(32):
-                 a = f.readline()
-                
-                 data = a.split()
-                 for k in range(len(data)):
-                    total = total + float(data[k])
-
-            print total
-            
-        print 64*64*64
-        f.close()
-    
-def testgrav():
-    x = [0, 	1.09936 ,	2.1949, 	3.28295 ,	4.36013 ,	5.42346 ,	6.47053 ,	7.49951 ,	8.5093 ,	9.49951 ,	10.4705 ,	11.4235 ,	12.3601 ,	13.283, 	14.1949 ,	15.0994 ,	16 ,	16.9006 ,	17.8051 ,	18.7171 ,	19.6399 ,	20.5765 ,	21.5295 ,	22.5005 ,	23.4907 ,	24.5005 ,	25.5295 ,	26.5765 ,	27.6399 ,	28.717, 	29.8051 ,	30.9006]
-
-    g = [-3.16758e-07 ,	0.445213 ,	1.23765 ,	1.96725 ,	2.6013 ,	3.11378, 	3.48665 ,	3.71026 ,	3.78302 ,	3.71041 ,	3.50347 ,	3.17716 ,	2.74871 ,	2.23623 ,	1.65776 ,	0.993836 ,	1.32012e-05, 	-0.993815 ,	-1.65774 ,	-2.23621 ,	-2.7487, 	-3.17715 ,	-3.50346 ,	-3.7104 ,	-3.78302 ,	-3.71027 ,	-3.48666 ,	-3.11379 ,	-2.60131 ,	-1.96727 ,	-1.23767 ,	-0.445227 ]
-
-    plt.clf()
-    fig = plt.figure(1)
-    sp = fig.add_subplot(111)
-
-    grav = []
-    for i in range(32):
-        grav.append(((3/(2 * 0.2))*(x[i] - float(i)))) 
-
-    sp.scatter(x,g)
-    sp.plot(x,grav)
-    plt.show()
 
     
